6bc9e58eb598030eb0d807a02fa4ad2ebe8b2398
SabarishCodeWizard <ravik60656@gmail.com>
2026-01-05 10:05:35 +0530

    first commits

--- build/Desktop_Qt_6_10_1-Debug/.qt/appsampleUI_res.cmake
+++ build/Desktop_Qt_6_10_1-Debug/.qt/appsampleUI_res.cmake
@@ -2,6 +2,10 @@
 set(target "appsampleUI")
 set(working_dir "/home/sabarish/sampleUI")
 set(src_and_dest_list
+    "/home/sabarish/sampleUI/README.md"
+    "/home/sabarish/sampleUI/build/Desktop_Qt_6_10_1-Debug/sampleUI/README.md"
+    "/home/sabarish/sampleUI/.gitignore"
+    "/home/sabarish/sampleUI/build/Desktop_Qt_6_10_1-Debug/sampleUI/.gitignore"
 
 )
 set(timestamp_file "/home/sabarish/sampleUI/build/Desktop_Qt_6_10_1-Debug/.qt/appsampleUI_res.txt")
